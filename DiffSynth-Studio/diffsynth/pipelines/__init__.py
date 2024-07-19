ba50dbd9cd17f13543733937c3ae9178763185e6
zhangxuecheng <517941852@qq.com>
2024-07-19 09:48:16 +0800

    exvideo commit

---
+++ DiffSynth-Studio/diffsynth/pipelines/__init__.py
@@ -0,0 +1,7 @@
+from .stable_diffusion import SDImagePipeline
+from .stable_diffusion_xl import SDXLImagePipeline
+from .stable_diffusion_video import SDVideoPipeline, SDVideoPipelineRunner
+from .stable_diffusion_xl_video import SDXLVideoPipeline
+from .stable_video_diffusion import SVDVideoPipeline
+from .hunyuan_dit import HunyuanDiTImagePipeline
+from .stable_diffusion_3 import SD3ImagePipeline
